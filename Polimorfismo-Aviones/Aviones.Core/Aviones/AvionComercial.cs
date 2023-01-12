7c41ddca4cfec9927c306b62d03c2622cb816845
Samuel Rivera <108555378+samuelerm08@users.noreply.github.com>
2023-01-12 13:24:31 -0300

    Update AvionComercial.cs

--- Polimorfismo-Aviones/Aviones.Core/Aviones/AvionComercial.cs
+++ Polimorfismo-Aviones/Aviones.Core/Aviones/AvionComercial.cs
@@ -7,26 +7,24 @@ using System.Threading.Tasks;
 namespace Aviones.Core.Aviones
 {
     public class AvionComercial : Avion
-    {
-        public AvionComercial(string piloto, string copiloto, string azafata, int capacidad, string lineaAerea) : base(piloto, copiloto, azafata)
-        {
-            Capacidad = capacidad;
-            LineaAerea = lineaAerea;
-        }
-
+    {        
         public int Capacidad { get; set; }
         public string LineaAerea { get; set; }
+        
         public override string Aterriza()
         {
             return "Avion Aterrizando";
         }
+        
         public override string Despegar()
         {
             return "Avion Despegando";
         }
+        
         public override string ToString()
         {
-            return base.VerPropiedades() + $"Capacidad: {Capacidad}\n" +
+            return base.VerPropiedades() + $"Azafata: {Azafata}\n" +
+                                            $"Capacidad: {Capacidad}\n" +
                                             $"Linea Aerea: {LineaAerea}";
         }
     }
