79c2735fa72d84f00d3ada7efb2adc49920a3bea
Samuel Rivera <108555378+samuelerm08@users.noreply.github.com>
2023-01-12 13:44:58 -0300

    Update Avion.cs

--- Polimorfismo-Aviones/Aviones.Core/Aviones/Avion.cs
+++ Polimorfismo-Aviones/Aviones.Core/Aviones/Avion.cs
@@ -6,15 +6,19 @@ using System.Threading.Tasks;
 
 namespace Aviones.Core.Aviones
 {
+    //Clase base
     public abstract class Avion
     {        
+        //Propiedades
         public string Piloto { get; set; }
         public string Copiloto { get; set; }
         public string Azafata { get; set; }
-
+    
+        //Metodos abstractos
         public abstract string Despegar();
         public abstract string Aterriza();
 
+        //Metodo virtual
         public virtual string VerPropiedades()
         {
             return $"Detalles Avion:\n" +
